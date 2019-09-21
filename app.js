f65e1a85080c3253bb77562f4bfe9592b62483a2
Benshuai Xu <bxu50@student.monash.edu>
2019-09-21 20:54:52 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const logModule = require('./module')
+const fs = require('fs')
+const http = require('http')
+const express = require('express')
+
+
+const app = express() 
+
+
+app.get('/carsales', function(req,res,next){
+    return res.json("hellooooo")
+})
+
+app.use((req,res,next)=>{
+    if(req.params['product'].indexof('mecca')>0){
+        res.send(error)
+    }
+    next()
+})
+app.get('/mecca/:product', function(req,res,next){
+    return res.send(`mecca ${req.params['product']}`)
+})
+
+
+
+app.listen(5666, function(){
+    console.log("service running on port 5666")
+})
+// http.createServer((req, res)=>{
+    
+//     if(req.url.indexOf('.css')>0){
+//         const cssContent = fs.readFileSync('./style.css')
+//         res.write(cssContent)
+//         res.end();
+//     } else{
+//         const content = fs.readFileSync('./index.html')
+//         res.write(content)
+//         res.end();
+//     }
+//     route.get('/carsales', function(req,res,next){
+//         res.write("hello")
+//     })
+
+// }).listen(5666);
+
+// logModule.logMessage('server is running on 5666...')
+
---
+++ script.js
@@ -0,0 +1,28 @@
+import module from './module'
+module.logFunction('hello')
+// let array = [0,1,2,3]
+// const filterResult = array.filter(i=> i<2)
+// console.log(filterResult)
+
+// const sum = array.reduce((sum, value)=>{
+//     return sum - value
+// })
+// console.log(sum)
+
+// const calendar = {
+//     currentDay: 6,
+//     nextDay: function(){
+//         setTimeout(()=>{
+//             console.log(this.currentDay+1)
+//         })
+//     }
+// }
+// calendar.nextDay();
+
+// //destructer
+// const array1 = ['jiangren', 'test1', 'test3'];
+// const [company, course] = array1;
+// console.log(company);
+
+
+
